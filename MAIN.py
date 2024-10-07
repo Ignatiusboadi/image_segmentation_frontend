703ae95dc61b6ebdeb8d80b490af0c4e99b9ad3d
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-07 10:42:19 +0000

    removed download button, segment button downloads segmented images

--- MAIN.py
+++ MAIN.py
@@ -1,19 +1,32 @@
+import uuid
+
 import dash
+import requests
 from dash import dcc, html, Input, Output, State, callback, dcc, ctx
+from app import api_url
 import dash_bootstrap_components as dbc
 import os
 import zipfile
 import io
 import base64
+import shutil
 
 from dash.exceptions import PreventUpdate
 from flask import send_file
 
-upload_message = 'Multiple file uploads are allowed. Please place all scans in a folder, select all files, and upload them together.'
 
-app = dash.Dash(__name__, title='IMAGE SEGMENTATION', external_stylesheets=[dbc.themes.SIMPLEX],
-                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}], update_title=None,
-                suppress_callback_exceptions=True, assets_folder='assets', assets_url_path='/assets/')
+def create_zip(file_data_list, filenames):
+    memory_file = io.BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        for data, name in zip(file_data_list, filenames):
+            decoded_data = base64.b64decode(data.split(',')[1])
+            zf.writestr(name, decoded_data)
+    memory_file.seek(0)
+    return memory_file
+
+
+upload_message = 'Multiple file uploads are allowed. Please place all scans in a folder, select all files, and upload them together.'
+UPLOAD_DIRECTORY = 'uploads'
 
 layout = html.Div(style={'padding-top': '30px', 'background-image': 'url("/assets/brain_imag_bg.webp"',
                          'height': '100vh'}, children=[
@@ -45,30 +58,16 @@ layout = html.Div(style={'padding-top': '30px', 'background-image': 'url("/asset
                         html.Em(upload_message, style={'color': 'green'}),
                         html.Br(),
                         dbc.Row(children=[dbc.Col(children=[
-                            dbc.Button("Segment Scans", id='segment', color="danger",
-                                       className='text-center', outline=True,
-                                       size='md', style={'padding-left': '45px',
-                                                         'padding-right': '45px'}), ],
-                            width={'offset': 4},
-                            style={'padding-left': '25px', 'padding-right': '25px'})],
+                            dbc.Button("Segment Scans", id='segment', color="danger", className='text-center',
+                                       outline=True, size='md',
+                                       style={'padding-left': '45px', 'padding-right': '45px'}),
+                            dcc.Loading(dcc.Download(id='download-btn'), fullscreen=True, )
+                        ],
+                            width={'offset': 4}, style={'padding-left': '25px', 'padding-right': '25px'})],
                             justify="center"),
                     ], ),
                 ], ),
-
             ], width={'size': 6, 'offset': 3})], style={'padding-bottom': '50px'}),
-        dbc.Row(style={'padding-top': '30px'}, children=[
-            dbc.Col(children=[
-                dbc.Card([
-                    dbc.CardBody(style={'background-color': 'GhostWhite'}, children=[
-                        dbc.Label("", html_for="upload-files"),
-                        dbc.Card([
-                            dbc.CardBody(style={'textAlign': 'center', }, children=[
-                                dbc.Button("Download segmented scans", id='segment', color="success",
-                                           outline=True, className="mt-1", size='md',
-                                           style={'padding-left': '45px', 'padding-right': '45px'}), ])]),
-                    ], ), ], ),
-
-            ], width={'size': 6, 'offset': 3})]),
     ], fluid=True)])
 
 
@@ -83,5 +82,57 @@ def log_out(n_clicks):
         return '/', None, None
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+@callback(Output('download-btn', 'data'),
+          Output('segment', 'n_clicks'),
+          Input('upload-files', 'filename'),
+          Input('upload-files', 'contents'),
+          Input('token', 'data'),
+          Input('segment', 'n_clicks'))
+def segment_images(file_names, file_contents, bearer_token, n_clicks):
+    if not n_clicks:
+        raise PreventUpdate
+    headers = {
+        'Authorization': f"Bearer {bearer_token}"
+    }
+    segment_api = f"{api_url}/prediction"
+    if n_clicks > 0 and file_contents is not None:
+        folder_name = str(uuid.uuid4())
+        folder_path = os.path.join(UPLOAD_DIRECTORY, folder_name)
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        for i, content in enumerate(file_contents):
+            content_type, content_string = content.split(',')
+            decoded_file = base64.b64decode(content_string)
+
+            with open(os.path.join(folder_path, file_names[i]), 'wb') as f:
+                f.write(decoded_file)
+
+        zip_filename = f"{folder_name}.zip"
+        zip_filepath = os.path.join(UPLOAD_DIRECTORY, zip_filename)
+
+        with zipfile.ZipFile(zip_filename, 'w') as zf:
+            for file_name in file_names:
+                zf.write(os.path.join(folder_path, file_name), file_name)
+        shutil.rmtree(folder_path)
+
+        try:
+            with open(zip_filename, "rb") as zip_file:
+                files = {
+                    "file": (zip_filename, zip_file, "application/zip")
+                }
+                response = requests.post(segment_api, headers=headers, files=files)
+                os.remove(zip_filename)
+        except FileNotFoundError:
+            return f"Error: The file {zip_filename} does not exist."
+    file_content = response.content
+    print(response.headers)
+    content_disp = response.headers.get('Content-Disposition')
+    file_name = content_disp.split('filename=')[1].strip('"')
+
+    return dcc.send_bytes(file_content, file_name), 0
+
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True, port=8051)
